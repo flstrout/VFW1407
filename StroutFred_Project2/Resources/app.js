f32aa8c92fdc926cd116f8ff6563412f11740604
fredstrout <fredstrout@freds-mbp.br.cox.net>
2014-07-15 00:08:50 -0500

    Navigation Window added

--- StroutFred_Project2/Resources/app.js
+++ StroutFred_Project2/Resources/app.js
@@ -6,31 +6,39 @@
 Ti.UI.setBackgroundColor('#000');
 
 // Data
-var listA = [{title: "Break Room"}, {title: "Front Counter Cooler"}, {title: "Brown Trays"}, {title: "Nugget Sauces"}, {title: "Salad Dressings"}, {title: "Candies & Toppings"}, {title: "++Wall Freezers"}, {title: "++Bathrooms"}];
-var listB = [{title: "ABS Cups & Lids"}, {title: "ABS Ice (if needed)"}, {title: "Drive Thru Cooler"}, {title: "Iced Teas"}, {title: "Service Trash"}, {title: "Service Floors"}, {title: "++HLZ's & Bagging Station"}, {title: "++BIM & Melitta Machines"}];
+var listA = [
+	{title: "Break Room", desc: "Clean"},
+	{title: "Front Counter Cooler", desc: "Clean"},
+	{title: "Brown Trays", desc: "Clean"},
+	{title: "Nugget Sauces", desc: "Clean"},
+	{title: "Salad Dressings", desc: "Clean"},
+	{title: "Candies & Toppings", desc: "Clean"},
+	{title: "++Wall Freezers", desc: "Clean"},
+	{title: "++Bathrooms", desc: "Clean"}
+];
+var listB = [{title: "ABS Cups & Lids", desc: "Clean"},
+	{title: "ABS Ice (if needed)", desc: "Clean"},
+	{title: "Drive Thru Cooler", desc: "Clean"},
+	{title: "Iced Teas", desc: "Clean"},
+	{title: "Service Trash", desc: "Clean"},
+	{title: "Service Floors", desc: "Clean"},
+	{title: "++HLZ's & Bagging Station", desc: "Clean"},
+	{title: "++BIM & Melitta Machines", desc: "Clean"}
+];
 
 var tableWindow = Ti.UI.createWindow({
+	title: "Other Tasks",
 	backgroundColor: "#ccc"
 });
 
-var titleView = Ti.UI.createView({
-	backgroundColor: "#fff",
-	top: 0,
-	height: 55
+var navWindow = Ti.UI.iOS.createNavigationWindow({
+	window: tableWindow
 });
 
 var border = Ti.UI.createView({
 	backgroundColor: "#333",
-	top: titleView.top + titleView.height,
+	top: 0,
 	height: 1
-});
-
-var titleLabel = Ti.UI.createLabel({
-	text: "Other Tasks",
-	font: {fontSize: 20, fontFamily: "Arial", fontWeight: "bold"},
-	top: titleView.top + titleView.height - 30,
-	width: "100%",
-	textAlign: "center"
 });
 
 var tasks = Ti.UI.createTableView({
@@ -51,14 +59,16 @@ var bListSection = Ti.UI.createTableViewSection({
 
 for(var i=0, j=listA.length; i<j; i++){
 	var sectionDetail = Ti.UI.createTableViewRow({
-		title: listA[i].title
+		title: listA[i].title,
+		detail: listA[i].desc
 	});
 	aListSection.add(sectionDetail);
 };
 
 for(var i=0, j=listB.length; i<j; i++){
 	var sectionDetail = Ti.UI.createTableViewRow({
-		title: listB[i].title
+		title: listB[i].title,
+		detail: listB[i].desc
 	});
 	bListSection.add(sectionDetail);
 };
@@ -67,6 +77,6 @@ var lists = [aListSection, bListSection];
 
 tasks.setData(lists);
 
-titleView.add(titleLabel);
-tableWindow.add(titleView, border, tasks);
-tableWindow.open();
+
+tableWindow.add(border, tasks);
+navWindow.open();
